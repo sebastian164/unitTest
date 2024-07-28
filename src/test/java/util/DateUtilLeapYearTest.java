51d68c328fcfb7ace75930375311d0d09cf297a6
Sebastian Charry <juan.charry@colombiacompra.gov.co>
2024-07-27 21:33:09 -0500

    add libraries and sonar to project

---
+++ src/test/java/util/DateUtilLeapYearTest.java
@@ -0,0 +1,16 @@
+package util;
+
+import org.junit.Test;
+
+public class dateUtilLeapYearTest {
+
+    /**
+     * All years divisible by 400 ARE Leap years (1600, 2000, 2400)
+     * All years divisible by 100 but not by 400 are NOT leap years (1700, 1800, 1900)
+     * All years divisible by 4 but not by 100 ARE 1eap years (1996, 2004, 2008)
+     * All years not divisible by 4 are NOT Leap years (2017, 2018, 2019)
+     */
+    @Test
+    public void return_true_when_xxxx() {
+    }
+}
